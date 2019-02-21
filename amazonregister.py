41446d236bd6f431c211511515fe10b486d0c11c
Joseph Aronson <aronsonjoey@gmail.com>
2019-02-20 16:24:09 -0800

    minor formatting changes

--- amazonregister.py
+++ amazonregister.py
@@ -67,33 +67,33 @@ class AmazonService(LocalBaseClass):
         return instancesFormatted
   
     def terminate_instance(self,index):
-      instances = self.formatted_instances
-      ids = [instances[index]['Instance Id']]
-      self.ec2.instances.filter(InstanceIds=ids).terminate()
-      print("Instance Terminated.")
-      print("Rerun %db to update.")
+        instances = self.formatted_instances
+        ids = [instances[index]['Instance Id']]
+        self.ec2.instances.filter(InstanceIds=ids).terminate()
+        print("Instance Terminated.")
+        print("Rerun %db to update.")
 
     def toggle_instance(self,index):
-      instances = self.formatted_instances
-      ids = [instances[index]['Instance Id']]
+        instances = self.formatted_instances
+        ids = [instances[index]['Instance Id']]
 
-      current_state = instances[index]['State']
-      if(current_state == "running"):
-        self.ec2.instances.filter(InstanceIds=ids).stop()
-        print("Instance Stopped.")
-        print("Rerun %db to update.")
+        current_state = instances[index]['State']
+        if(current_state == "running"):
+            self.ec2.instances.filter(InstanceIds=ids).stop()
+            print("Instance Stopped.")
+            print("Rerun %db to update.")
 
-      elif(current_state == "stopped"):
-        self.ec2.instances.filter(InstanceIds=ids).start()
-        print("Instance Started.")
-        print("Rerun %db to update.")
+        elif(current_state == "stopped"):
+            self.ec2.instances.filter(InstanceIds=ids).start()
+            print("Instance Started.")
+            print("Rerun %db to update.")
 
     def reboot_instance(self,index):
-      instances = self.formatted_instances
-      ids = [instances[index]['Instance Id']]
-      self.ec2.instances.filter(InstanceIds=ids).reboot()
-      print("Instance Rebooted.")
-      print("Rerun %db to update.")
+        instances = self.formatted_instances
+        ids = [instances[index]['Instance Id']]
+        self.ec2.instances.filter(InstanceIds=ids).reboot()
+        print("Instance Rebooted.")
+        print("Rerun %db to update.")
     
 if __name__ == '__main__':
     print('SubClass:', issubclass(AmazonService,
