b0073398e7525a9b53f3afdb740a7a6cdac83ae0
Joseph Aronson <aronsonjoey@gmail.com>
2019-04-29 14:39:04 -0700

    added groups to ssh command

--- user_interfaces/SshInterface.py
+++ user_interfaces/SshInterface.py
@@ -2,6 +2,8 @@ from IPython.core import magic_arguments
 from IPython.core.magic import line_magic, cell_magic, line_cell_magic, Magics, magics_class
 from IPython.display import HTML, display, FileLink
 import ipywidgets as widgets
+from ipywidgets import Layout, Button, Box, FloatText, Textarea, Dropdown, Label, IntSlider
+
 
 import plugins
 import paramiko
@@ -13,20 +15,54 @@ warnings.filterwarnings(action='ignore',module='.*paramiko.*')
 #--------------SSH-Interface-Function---------------#
 #===================================================#
 def render_ssh_interface(cloud_list):
-	box_layout = widgets.Layout(
-				border='solid 1px',
-				# padding='0.5em',
-				margin='0.5em',
-				width = '28%',
-				
-			)
+	title = widgets.HTML("<h4>SSH into instances </h4>")
+	display(title)
 	instances = cloud_list[0].get_instances_info()
+	group_list = []
+
+	for instance in instances:
+		group_name = instance['Group Name']
+		if group_name not in group_list and group_name != '':
+			group_list.append(group_name)
+
+	group_list.sort(key=str.lower)
+	tab_arr = []
+	layout_arr = render_group(instances,'All Instances')
+	tab_child = widgets.VBox(layout_arr)
+	tab_arr.append(tab_child)
+
+	tab = widgets.Tab()
+	for group_name in group_list:
+		layout_arr = render_group(instances,group_name)
+		tab_child = widgets.VBox(layout_arr)
+		tab_arr.append(tab_child)
+	
+	tab.children = tab_arr
+	tab.set_title(0,'All Instances')
+	# set titles for tab
+	for i in range(len(group_list)):
+		tab.set_title(i+1, group_list[i])
+
+	display(tab)
+	
+
+def render_group(instances,group_name):
+	group_layout_arr = []
+
+	# box_layout = widgets.Layout(
+	# 		border='solid 1px',
+	# 		# padding='0.5em',
+	# 		margin='0.5em',
+	# 		width = '28%',
+			
+		# )
+	# instances = cloud_list[0].get_instances_info()
 	box_list = []
 	instance_boxes = []
 
 	#creates the checkboxes
 	for vm in instances:
-		if(vm['State'] == 'running'):
+		if(vm['State'] == 'running' and (vm['Group Name'] == group_name or group_name == 'All Instances')):
 			if(vm['Name'] == ''):	
 				cb = widgets.Checkbox(
 					value=True,
@@ -39,7 +75,7 @@ def render_ssh_interface(cloud_list):
 					description=vm['Name'],
 					disabled=False
 				)
-			instance_boxes.append(widgets.Box([cb], layout=box_layout))
+			instance_boxes.append(cb)
 			box_list.append(cb)
 	
 	select_button = widgets.Button(
@@ -56,9 +92,14 @@ def render_ssh_interface(cloud_list):
 	)
 	
 	if(len(box_list) == 0):
-		display(widgets.HTML(value="There are no running instances."))
+		if(group_name != "All Instances"):
+			group_layout_arr.append(widgets.HTML(value="There are no running instances in " + group_name))
+		else:
+			group_layout_arr.append(widgets.HTML(value="There are no running instances"))
+
 	else:
-		display(select_button)
+		# display(select_button)
+		group_layout_arr.append(select_button)
 
 		box_array = []
 
@@ -67,14 +108,20 @@ def render_ssh_interface(cloud_list):
 		
 		for row in box_array:
 			boxes_container = widgets.HBox(row)
-			display(boxes_container)
+			# display(boxes_container)
+			group_layout_arr.append(boxes_container)
 		
 		command_area = widgets.Textarea(
 			value='',
 			placeholder='Type your commands here',
+			layout=Layout(width='auto'),
 		)
 		command_box = widgets.VBox([command_area,submit_button])
-		display(command_box)
+		# display(command_box)
+		group_layout_arr.append(command_box)
+		
+
+	
 
 	#===================================================#
 	#-----------------Button-Functions------------------#
@@ -136,3 +183,4 @@ def render_ssh_interface(cloud_list):
 
 	submit_button.on_click(submit_button_clicked)
 	select_button.on_click(select_button_clicked)
+	return group_layout_arr
